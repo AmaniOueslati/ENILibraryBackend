c4e8de9114cfa54155f8184d1693e14670df0fe9
Amani <weslatiamani86@gmail.com>
2024-04-10 17:00:06 +0200

    first commit

---
+++ src/main/java/enilibrary/EniLibrary/EniLibraryApplication.java
@@ -0,0 +1,13 @@
+package enilibrary.EniLibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EniLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EniLibraryApplication.class, args);
+	}
+
+}
